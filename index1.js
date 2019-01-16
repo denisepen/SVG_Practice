9cc99e3ce552e4a54c89bc0431d91d0b065b83d4
Denise Pendleton-Lipinski <denisependletonlipinski@Denises-MacBook-Pro.local>
2019-01-16 14:28:49 -0500

    added rectangele objects to the dom

--- index1.js
+++ index1.js
@@ -2,13 +2,15 @@
 // alert("Hello")
 
 const data = [
-{width: 200, height: 700, fill: 'orange'}
+{width: 200, height: 100, fill: 'purple'},
+{width: 100, height: 60, fill: 'pink'},
+{width: 50, height: 30, fill: 'orange'}
 ]
 
 // select the svg container first
 const svg = d3.select('svg');
 
-const rect = svg.select('rect')
+const rect = svg.selectAll('rect')
     .data(data)
     .attr('width', (d, i, n) => d.width )
     .attr('height', (d) => d.height  )
